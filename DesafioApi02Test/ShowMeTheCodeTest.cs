39b295f2a3cb3544cd4f3f1d5d967fa0b9cee5b6
Lucas Pompeu Neves <lucaspompeuneves@gmail.com>
2021-07-15 09:55:11 -0300

    Implementação teste do show me the code

---
+++ DesafioApi02Test/ShowMeTheCodeTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesafioApi02.Service;
+using DesafioApi02.Model;
+
+namespace DesafioApi02Test
+{
+    [TestClass]
+    class ShowMeTheCodeTest
+    {
+        public string UrlDesafio01 { get; set; }
+        public string UrlDesafio02 { get; set; }
+        public ShowMeTheCode UrlsGitHub { get; set; }
+
+        public ShowMeTheCodeTest()
+        {
+            UrlDesafio01 = "https://github.com/lucaspompeun/DesafioApi01";
+            UrlDesafio02 = "https://github.com/lucaspompeun/DesafioApi02";
+
+            UrlsGitHub = new ShowMeTheCode
+            {
+                UrlDesafio01 = this.UrlDesafio01,
+                UrlDesafio02 = this.UrlDesafio02
+            };
+        }
+
+        [TestMethod]
+        public void IntegridadeUrl01()
+        {
+            var actual = ShowMeTheCodeService.GetUrlsGithub();
+            var expected = UrlDesafio01;
+
+
+            Assert.AreEqual(expected, actual.UrlDesafio01);
+        }
+
+        [TestMethod]
+        public void IntegridadeUrl02()
+        {
+            var actual = ShowMeTheCodeService.GetUrlsGithub();
+            var expected = UrlDesafio02;
+
+
+            Assert.AreEqual(expected, actual.UrlDesafio02);
+        }
+    }
+}
